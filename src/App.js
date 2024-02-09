08acfd3cee9d4152b82eaf47b35c52aaf1d56121
ditya17 <nitya17nitya@gmail.com>
2024-02-09 23:51:43 +0530

    base-repo-setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-black h-[100vh]'>
+      <h className="text-white">Welcome to Temple of Learning</h>
+    </div>
+  );
+}
+
+export default App;
